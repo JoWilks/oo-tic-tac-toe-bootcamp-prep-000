dce3194453b16289b04496584787177479fa96ac
joannewilkins88@gmail.com <joannewilkins88@gmail.com>
2018-06-28 12:01:08 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,6 +1,13 @@
 class Tic_Tac_Toe
 
-WIN_COMBINATIONS = [ [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6] ]
+WIN_COMBINATIONS = [ [0,1,2], 
+[3,4,5], 
+[6,7,8], 
+[0,3,6], 
+[1,4,7], 
+[2,5,8], 
+[0,4,8], 
+[2,4,6] ]
 
   def initialize(board)
     @board = board
