6765722420575459ea6be190ea0f0becad5aa58c
joannewilkins88@gmail.com <joannewilkins88@gmail.com>
2018-06-28 12:08:22 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -31,10 +31,10 @@ WIN_COMBINATIONS = [
   end
 
   def position_taken?(index)
-   !(board[index].nil? || board[index] == " ")
+   !(@board[index].nil? || @board[index] == " ")
   end
   
-  def valid_move?(board, index)
-    index.between?(0,8) && !position_taken?(board, index)
+  def valid_move?(index)
+    index.between?(0,8) && !position_taken?(index)
   end
 end
