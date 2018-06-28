766d0c1f251e11aee70afa10daddd7f3aca3bab0
joannewilkins88@gmail.com <joannewilkins88@gmail.com>
2018-06-28 12:11:16 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -51,11 +51,19 @@ WIN_COMBINATIONS = [
       end
   end
   
+  def turn_count() 
+    move_count = 0
+    @board.each do |position|
+      if position == "X" || position == "O"
+        move_count += 1
+      end
+     end
+    return move_count
+  end
   
   
-  
-  def current_player(board)
-    move_count = turn_count(board)
+  def current_player()
+    move_count = turn_count()
     if move_count % 2 == 0
       "X"
     else
